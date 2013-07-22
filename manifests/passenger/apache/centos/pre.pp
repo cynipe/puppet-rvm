1da98c97f4eed1c531737e92148f5627da329845
cynipe <cynipe@gmail.com>
2013-07-22 16:28:42 +0900

    Temporary removed httpd, since it's not work with puppetlabs/apache

--- manifests/passenger/apache/centos/pre.pp
+++ manifests/passenger/apache/centos/pre.pp
@@ -1,6 +1,7 @@
 class rvm::passenger::apache::centos::pre {
   # Dependencies
-  if ! defined(Package['httpd'])       { package { 'httpd':       ensure => present } }
+  # TODO no idea why httpd conflicts with puppetlabs/apache
+  # if ! defined(Package['httpd'])       { package { 'httpd':       ensure => present } }
   if ! defined(Package['httpd-devel']) { package { 'httpd-devel': ensure => present } }
   if ! defined(Package['mod_ssl'])     { package { 'mod_ssl':     ensure => present } }
 }
